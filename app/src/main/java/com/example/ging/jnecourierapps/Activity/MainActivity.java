35fa3eee0a3fc03169cc71804ebe4a80a7148dbe
Ismail Syarief <ismail8syarief@gmail.com>
2019-01-07 15:12:06 +0700

    refactor

--- app/src/main/java/com/example/ging/jnecourierapps/Activity/MainActivity.java
+++ app/src/main/java/com/example/ging/jnecourierapps/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.ging.jnecourierapps;
+package com.example.ging.jnecourierapps.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ging.jnecourierapps.R;
 
 public class MainActivity extends AppCompatActivity {
 
