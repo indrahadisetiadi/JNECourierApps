2be76f2ed9b2a04de8b6a0aba6ba01f7a1b46f35
Unknown <raditya113@icloud.com>
2019-01-07 16:22:26 +0700

    login screen

--- app/src/main/java/com/example/ging/jnecourierapps/LoginActivity.java
+++ app/src/main/java/com/example/ging/jnecourierapps/LoginActivity.java
@@ -1,17 +1,21 @@
 package com.example.ging.jnecourierapps;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.Gravity;
 import android.graphics.Color;
 import android.content.DialogInterface;
 import android.widget.LinearLayout;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -31,31 +35,27 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void openDialog(){
-//        final Spinner sp = new Spinner(LoginActivity.this);
-//        sp.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
 
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        // Set Message
+        LinearLayout layout = new LinearLayout(LoginActivity.this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(20,20,20,20);
+
+        //progress bar
+        ProgressBar progressBar = new ProgressBar(LoginActivity.this, null, android.R.attr.progressBarStyleLarge);
+        layout.addView(progressBar);
+
+        //progress text
         TextView msg = new TextView(this);
-        // Message Properties
         msg.setText("Harap Tunggu");
+        msg.setPadding(10, 80, 10, 30);
         msg.setGravity(Gravity.CENTER_HORIZONTAL);
         msg.setTextColor(Color.BLACK);
-        alertDialog.setView(msg);
+        layout.addView(msg);
 
-        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE,"Batal", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                // Perform Action on Button
-            }
-        });
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setView(layout);
 
         new Dialog(getApplicationContext());
         alertDialog.show();
-
-        final Button cancelBT = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
-        LinearLayout.LayoutParams negBtnLP = (LinearLayout.LayoutParams) cancelBT.getLayoutParams();
-        negBtnLP.gravity = Gravity.FILL_HORIZONTAL;
-        cancelBT.setTextColor(Color.RED);
-        cancelBT.setLayoutParams(negBtnLP);
     }
 }
