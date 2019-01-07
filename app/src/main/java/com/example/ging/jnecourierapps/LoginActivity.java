c0a64e3a5e354f9544e7902c891d1c337f20d92f
Unknown <raditya113@icloud.com>
2019-01-07 15:13:58 +0700

    Layout Login, kurang spinner + efek2

--- app/src/main/java/com/example/ging/jnecourierapps/LoginActivity.java
+++ app/src/main/java/com/example/ging/jnecourierapps/LoginActivity.java
@@ -1,16 +1,61 @@
 package com.example.ging.jnecourierapps;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.graphics.Color;
+import android.content.DialogInterface;
+import android.widget.LinearLayout;
+
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button LoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        ConstraintLayout.(ContextCompat.getColor(this,R.color.colorPrimary));
+        LoginButton = findViewById(R.id.login);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    private void openDialog(){
+//        final Spinner sp = new Spinner(LoginActivity.this);
+//        sp.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        // Set Message
+        TextView msg = new TextView(this);
+        // Message Properties
+        msg.setText("Harap Tunggu");
+        msg.setGravity(Gravity.CENTER_HORIZONTAL);
+        msg.setTextColor(Color.BLACK);
+        alertDialog.setView(msg);
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE,"Batal", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // Perform Action on Button
+            }
+        });
+
+        new Dialog(getApplicationContext());
+        alertDialog.show();
+
+        final Button cancelBT = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        LinearLayout.LayoutParams negBtnLP = (LinearLayout.LayoutParams) cancelBT.getLayoutParams();
+        negBtnLP.gravity = Gravity.FILL_HORIZONTAL;
+        cancelBT.setTextColor(Color.RED);
+        cancelBT.setLayoutParams(negBtnLP);
     }
 }
--- app/src/main/java/com/example/ging/jnecourierapps/MainActivity.java
+++ app/src/main/java/com/example/ging/jnecourierapps/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.ging.jnecourierapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.graphics.Color;
+import android.content.DialogInterface;
+import android.widget.LinearLayout;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button LoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginButton = findViewById(R.id.login);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
     }
+
+    private void openDialog(){
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        // Set Message
+        TextView msg = new TextView(this);
+        // Message Properties
+        msg.setText("I am a Custom Dialog Box. \n Please Customize me.");
+        msg.setGravity(Gravity.CENTER_HORIZONTAL);
+        msg.setTextColor(Color.BLACK);
+        alertDialog.setView(msg);
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE,"Batal", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // Perform Action on Button
+            }
+        });
+
+        new Dialog(getApplicationContext());
+        alertDialog.show();
+
+        final Button cancelBT = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        LinearLayout.LayoutParams negBtnLP = (LinearLayout.LayoutParams) cancelBT.getLayoutParams();
+        negBtnLP.gravity = Gravity.FILL_HORIZONTAL;
+        cancelBT.setTextColor(Color.RED);
+        cancelBT.setLayoutParams(negBtnLP);
+    }
+
 }
