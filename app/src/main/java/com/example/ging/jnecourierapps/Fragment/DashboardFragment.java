cedcf0d857a59d9326b615a50e08add8fa84fc1d
Ismail Syarief <ismail8syarief@gmail.com>
2019-01-08 11:54:26 +0700

    change bottom menu to history

--- app/src/main/java/com/example/ging/jnecourierapps/Fragment/DashboardFragment.java
+++ app/src/main/java/com/example/ging/jnecourierapps/Fragment/DashboardFragment.java
@@ -1,7 +1,5 @@
 package com.example.ging.jnecourierapps.Fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,6 +13,6 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return inflater.inflate(R.layout.fragment_history, container, false);
     }
 }
