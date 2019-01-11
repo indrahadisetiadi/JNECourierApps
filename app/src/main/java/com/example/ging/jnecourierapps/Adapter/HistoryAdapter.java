0ff380674f7830361eb61d5a8ff9fddc750186f0
Hilmi Raditya <raditya113@icloud.com>
2019-01-11 10:20:50 +0700

    solve yang conflig2

--- app/src/main/java/com/example/ging/jnecourierapps/Adapter/HistoryAdapter.java
+++ app/src/main/java/com/example/ging/jnecourierapps/Adapter/HistoryAdapter.java
@@ -33,15 +33,6 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.history_list_card, viewGroup, false);
         ViewHolder holder = new ViewHolder(v);
-//        history_card = viewGroup.findViewById(R.id.history_card);
-//        history_card.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Log.i("HEHEHE", "BISA DONG BROOOOOOOOO");
-////                BottomSheetDialogFragment bottomSheetDialogFragment = new HistoryBottomDialog();
-////                bottomSheetDialogFragment.show(((MainActivity)mContext).getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
-//            }
-//        });
         return holder;
     }
 
@@ -60,10 +51,13 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
-            itemView.setOnClickListener(new View.OnClickListener() {
+            history_card = itemView.findViewById(R.id.history_card);
+            history_card.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Log.i("HEHEHE", "BISA DONG BROOOOOOOOO");
+                BottomSheetDialogFragment bottomSheetDialogFragment = new HistoryBottomDialog();
+                bottomSheetDialogFragment.show(((MainActivity)mContext).getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
                 }
             });
         }
