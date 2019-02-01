26be1e39e953273daf422048c087fc1ada6115a6
Hilmi Raditya <raditya113@icloud.com>
2019-02-01 09:26:29 +0700

    ui fix

--- app/src/main/java/com/example/ging/jnecourierapps/Url/BaseUrl.java
+++ app/src/main/java/com/example/ging/jnecourierapps/Url/BaseUrl.java
@@ -2,8 +2,8 @@ package com.example.ging.jnecourierapps.Url;
 
 public class BaseUrl {
 //    private String Url = "http://10.2.172.221/JNECourierOptimazion/public/api/";
-//    private String Url = "http://10.1.9.240:8000/api/";
-    private String Url = "http://172.20.10.3:8000/";
+    private String Url = "http://10.1.9.240:8000/";
+//    private String Url = "http://172.20.10.3:8000/";
 //    private String Url = "http://10.1.8.158:1001/api/";
 //private String Url = "http://127.0.0.1:8000/api/";
 //    private String Url = "http://127.0.0.1:8000/api/";
